8d2de2a7de5142bd7862962353340a82536cd461
Cloud User <sriramkauik@gmail.com>
2023-12-02 13:50:28 +0000

    Ec2

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+  #access_key =   "AKIAT7G5PJOZSF7XPWQM"
+  #secret_key = "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"
+  
+}
+
+variable "ami_value" {
+  #default = "ami-053b0d53c279acc90"
+  
+}
+
+variable "it" {
+  #default = "t2.micro"
+  
+}
+
+resource "aws_instance" "Ec2module" {
+    ami = var.ami_value
+    instance_type = var.it
+   
+}
