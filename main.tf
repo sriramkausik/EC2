6e39e2f17eedd83b7e0c4db02dcd24df0287f6e4
sriramkausik <45325612+sriramkausik@users.noreply.github.com>
2023-12-02 17:11:05 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "ami_value" {
+    description = "value for the ami"
+}
+
+variable "instance_type_value" {
+    description = "value for instance_type"
+}
+
+variable "subnet_id_value" {
+    description = "value for the subnet_id"
+}
+
 resource "aws_instance" "example" {
     ami = var.ami_value
     instance_type = var.instance_type_value
