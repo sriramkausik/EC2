1cb0001238a3517a9f1875138c5aa5e088e0304f
sriramkausik <45325612+sriramkausik@users.noreply.github.com>
2023-12-02 16:55:42 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,9 @@
 provider "aws" {
   region = "us-east-1"
-  #access_key =   "AKIAT7G5PJOZSF7XPWQM"
-  #secret_key = "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"
-  
 }
 
-/*variable "ami_value" {
-  #default = "ami-053b0d53c279acc90"
-  
-}
-
-variable "it" {
-  #default = "t2.micro"
-  
-}
-
-variable "subnetid_value" {
-  #default = "t2.micro"
-  
-}*/
-
-resource "aws_instance" "Ec2module" {
+resource "aws_instance" "example" {
     ami = var.ami_value
-    instance_type = var.it
-    subnet_id = var.subnetid_value
-   
+    instance_type = var.instance_type_value
+    subnet_id = var.subnet_id_value
 }
