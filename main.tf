d4e2ebe3a332fd8687808e7759a16b217c91a336
sriramkausik <45325612+sriramkausik@users.noreply.github.com>
2023-12-03 08:49:09 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,5 +17,5 @@ variable "subnet_id_value" {
 resource "aws_instance" "example" {
     ami = var.ami_value
     instance_type = var.instance_type_value
-    subnet_id = subnet-0aab0a5e16287ebdb
+    subnet_id = var.subnet_id_value
 }
